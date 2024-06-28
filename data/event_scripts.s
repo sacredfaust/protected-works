849dfb69f30b91fe10b5963f939c7211e34035f0
Faust <sacredfaust@gmail.com>
2024-06-27 21:15:44 -0400

    Set-Up

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1697,3 +1697,5 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+
+	.include "data/maps/YamHills/scripts.inc"
